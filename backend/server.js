63c5eba0b51e9cc61b0cd5175672b1dbc5719ce1
savidyainduma <Savidyainduma@gmail.com>
2023-11-22 17:09:47 +0530

    finished

---
+++ backend/server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db =  mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "signup"
+})
+
+app.post('/signup', (req, res) => {
+    const sql = "INSERT INTO login (name, email, password, mobile, city) VALUES (?)";
+    const values = [
+        req.body.name,
+        req.body.email,
+        req.body.password,
+        req.body.mobile,
+        req.body.city
+    ]
+    db.query(sql, [values], (err, data) => {
+        if(err) {
+            return res.json("Error");
+        }
+        return res.json(data);
+    })
+})
+
+app.post('/login', (req, res) => {
+    const sql = "SELECT * FROM login WHERE email = ? AND password = ?";
+    db.query(sql, [req.body.email,  req.body.password], (err, data) => {
+        if(err) {
+            return res.json("Error");
+        }
+        if(data.length > 0) {
+            return res.json("Success");
+        }
+        else {
+            return res.json("Failed");
+        }
+    })
+})
+
+app.get('/home', (req, res) => {
+    const sql = "SELECT * FROM login";
+    db.query(sql, (err, data) => {
+        if(err){ return res.json(err)
+            return res.status(500).json({ error: "Error occurred during fetching data from the database" });
+        }
+        return res.json(data);
+    })
+})
+
+app.listen(8081, () => {
+    console.log("listning");
+})
